fb71e06095365b6084478e60deb51360adb25002
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-10 19:23:48 +0000

    Done.

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,11 @@
+class Show < ActiveRecord::Migration[5.2]
+  
+  def change
+    create_table :shows do |s|
+      s.string :name
+      s.string :network
+      s.string :day
+      s.integer :rating
+    end
+  end
+end
